5f2f690eeb8b469a4f6f64d9dd26ceec35d4af5c
mloyen <mloyen@1ceaf258-81e9-0310-a0ff-b12b37f2a969>
2004-12-02 14:13:39 +0000

    Premier commit de test

---
+++ PROJECT/src/fr/umlv/easytime/test/test.java
@@ -0,0 +1,20 @@
+/*
+ * Created on 2 déc. 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package fr.umlv.easytime.test;
+
+/**
+ * @author mloyen
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
