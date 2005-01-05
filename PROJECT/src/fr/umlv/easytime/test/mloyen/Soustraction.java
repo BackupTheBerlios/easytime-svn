a96b530e4cba1379ec4ffa32f8048b7a7fd994db
mloyen <mloyen@1ceaf258-81e9-0310-a0ff-b12b37f2a969>
2005-01-05 14:59:32 +0000

    Mon test bidon

---
+++ PROJECT/src/fr/umlv/easytime/test/mloyen/Soustraction.java
@@ -0,0 +1,25 @@
+/*
+ * Created on 5 janv. 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package fr.umlv.easytime.test.mloyen;
+
+/**
+ * @author mloyen
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Soustraction {
+	
+	
+	static int soutrait(int i, int j){
+		return i-j;
+		
+	}
+
+	public static void main(String[] args) {
+	}
+}
